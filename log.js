102fc9cbe73b40eba551136108ea24bb04d15489
Tim Park <timfpark@gmail.com>
2018-03-27 09:16:53 -0700

    dump pushing logs - move to fluentd approach

--- log.js
+++ log.js
@@ -1,95 +1,31 @@
-const request = require('request');
-
-const LOGS_ENDPOINT = "https://logs.rhom.io/logs";
-const LOG_POST_INTERVAL = 5000;
-
-function buildPathTags(path) {
-    let pathTags = [];
-    let currentPath = "";
-    let pathParts = path.split('/');
-    let prefix = "";
-
-    pathParts.forEach(pathPart => {
-        currentPath += prefix + pathPart;
-        prefix = "/";
-        pathTags.push(`path:${currentPath}`);
-    });
-
-    return pathTags;
-}
-
 class Log {
     constructor(options) {
         for (let key in options) {
             this[key] = options[key];
         }
-
-        this.queued = [];
-
-        setInterval(() => {
-            this.post();
-        }, LOG_POST_INTERVAL);
-    }
-
-    clone(path) {
-        return new Log({
-            source: this.source,
-            path,
-            severity: this.severity
-        });
     }
 
     debug(message) {
-        if (['debug'].indexOf(this.severity) !== -1)
-            this.log('DEBUG', message);
+        if (["debug"].indexOf(this.severity) !== -1) this.log("DEBUG", message);
     }
 
     info(message) {
-        if (['debug','info'].indexOf(this.severity) !== -1)
-            this.log('INFO', message);
+        if (["debug", "info"].indexOf(this.severity) !== -1)
+            this.log("INFO", message);
     }
 
     warn(message) {
-        if (['debug','info','warn'].indexOf(this.severity) !== -1)
-            this.log('WARN', message);
+        if (["debug", "info", "warn"].indexOf(this.severity) !== -1)
+            this.log("WARN", message);
     }
 
     error(message) {
-        if (['debug', 'info', 'warn', 'error'].indexOf(this.severity) !== -1)
-            this.log('ERROR', message);
+        if (["debug", "info", "warn", "error"].indexOf(this.severity) !== -1)
+            this.log("ERROR", message);
     }
 
     log(severity, message) {
-        console.log(`${new Date().toISOString()}: ${severity}: ${this.path}: ${message}`);
-        this.queue({
-            severity,
-            tier: process.env.LOG_TIER,
-            source: this.source,
-            log: message,
-            timestamp: Date.now()
-        });
-    }
-
-    queue(event) {
-        event.tags = buildPathTags(this.path);
-        event.tags.push(`source:${this.source}`);
-
-        this.queued.push(event);
-    }
-
-    post() {
-        if (this.queued.length === 0) return;
-
-        request.post(LOGS_ENDPOINT, {
-            body: {
-                logs: this.queued
-            },
-            json: true
-        }, err => {
-            if (err) console.log(err);
-        });
-
-        this.queued = [];
+        console.log(`${severity}: ${this.path}: ${message}`);
     }
 }
 
--- test/log.js
+++ test/log.js
@@ -1,19 +1,19 @@
-"use strict"
+"use strict";
 
-const assert = require('assert'),
-      Log = require('../log.js')
+const assert = require("assert"),
+    Log = require("../log.js");
 
 let log = new Log({
-    severity: 'info',
-    source: 'testService',
-    path: 'test/path'
+    severity: "info",
+    source: "testService",
+    path: "test/path"
 });
 
-describe('log client', function() {
-    it('can create log', function(done) {
-        log.info('Hello world!');
+describe("log client", function() {
+    it("can create log", function(done) {
+        log.info("Hello world!");
+        log.debug("Not printed!");
 
-        // wait for log to be sent.
-        setTimeout(done, 6000);
+        done();
     });
 });
